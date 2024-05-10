e0f311d1b579283772cea4ced59171f277d5c850
opilane <merilin.hiis13@gmail.com>
2024-05-10 12:19:30 +0300

    ül

---
+++ inheritence/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert 2 numbers ");
+            Console.WriteLine("First number ");
+            int firstNr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second number ");
+            int secondNr = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(firstNr);
+            rectangle.SetHeight(secondNr);
+
+            Console.WriteLine("Total area :{0}", rectangle.GetArea());
+        }
+    }
+
+    class Shapes
+    {
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+
+        public void SetHeight(int h) 
+        {
+            height = h;
+        }
+
+        protected int height;
+        protected int width;
+    }
+
+    class Rectangle : Shapes
+    {
+        public int GetArea() 
+        {
+            return (width * height);
+        }
+    }
+}
