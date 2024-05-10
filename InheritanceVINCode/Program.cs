168c7f0f8d0a28a788dcc2fd7c8e21d3202bcca7
opilane <merilin.hiis13@gmail.com>
2024-05-10 13:37:57 +0300

    MA TEGIN ISE

---
+++ InheritanceVINCode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace InheritanceVINCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta masina kood");
+            int vincode = Convert.ToInt32(Console.ReadLine());
+            
+            Machine machine = new Machine();
+            machine.VinCode(vincode);
+            Console.WriteLine("Cool vincode: {0}", machine.VIN());
+        }
+    }
+    class Car
+    {
+        public void VinCode(int v)
+        {
+            vin = v;
+            
+        }
+        protected int vin;
+    }
+
+    class Machine : Car
+    {
+        public int VIN()
+        {
+            return vin;
+        }
+    }
+}
